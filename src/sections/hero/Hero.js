a97bc93d79ed44c6e45bae829d13e8f8b59d3eca
Paolo Espejo <u20211b786@upc.edu.pe>
2025-11-11 03:50:23 -0500

    feat: Hero section created

---
+++ src/sections/hero/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+import Button from '../../components/button/Button'
+
+export const Hero = () => {
+  return (
+    <section className='hero-section'>
+        <div className='content-container'>
+            <h1>Ingeniero de Software</h1>
+            <h2>Fullstack Junior</h2>
+            <p>Desarrollador apasionado por crear soluciones web modernas y escalables.</p>
+            <div className='button-container'>
+                <Button type='primary'>Ver proyectos</Button>
+                <Button type='secondary'>Contactar</Button>
+            </div>
+        </div>
+    </section>
+  )
+}
