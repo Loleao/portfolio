528b387c0e331ae4ed0fafc82ad37caa077be909
Paolo Espejo <u20211b786@upc.edu.pe>
2025-11-12 06:04:26 -0500

    feat: Updated Hero section

--- src/sections/hero/Hero.js
+++ src/sections/hero/Hero.js
@@ -5,11 +5,11 @@ import Button from '../../components/button/Button'
 export const Hero = () => {
   return (
     <section className='hero-section'>
-        <div className='content-container'>
+        <div className='hero-content-container'>
             <h1>Ingeniero de Software</h1>
             <h2>Fullstack Junior</h2>
             <p>Desarrollador apasionado por crear soluciones web modernas y escalables.</p>
-            <div className='button-container'>
+            <div className='hero-button-container'>
                 <Button type='primary'>Ver proyectos</Button>
                 <Button type='secondary'>Contactar</Button>
             </div>
