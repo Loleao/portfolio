379b8dd1bc9adf90cfc5e36b7c46ef74b9d47e76
Paolo Espejo <u20211b786@upc.edu.pe>
2025-11-12 06:05:01 -0500

    chore: Added About in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import { Navbar } from './components/navbar/Navbar';
+import { About } from './sections/about/About';
 import { Hero } from './sections/hero/Hero';
 
 function App() {
   return (
-    <>
+    <div className='app-container'>
       <Navbar/>
-      <Hero/>
-    </>
+      <main>
+        <section>
+          <Hero/>
+        </section>
+        <section>
+          <About/>
+        </section>
+      </main>
+    </div>
   );
 }
 
